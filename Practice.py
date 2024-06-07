c61a0a9c9c0852978e25ccd5db017a482fb1513e
tazzwarr <ttrockker3767@gmail.com>
2024-06-07 11:31:57 +0600

    Add files via upload

--- Practice.py
+++ Practice.py
@@ -1,48 +1,54 @@
-print("Welcome to the Game made by Nadid & Tazwar")
+import random
 
-playing = input("would you like to play? : ")
 
-if playing.lower() == "no":
-    print("OK, Please exit")
-    quit()
+def choose():
+    min_value = 1
+    max_value = 5
+    choose = random.randint(min_value, max_value)
 
-print("Well to the game, Get Ready for the first question")
-score = 0
+    return choose
 
-answer = input("Question 1 - What is the full form of CR7? ans: ")
-if answer.lower() == "cristiano ronaldo 7":
-    print("Correct")
-    score +=1
-else:
-    print("Incorrect")
 
-answer = input("Question 2 - What is the full form of LM10? ans: ")
-if answer.lower() == "lionel messi 10":
-    print("Correct")
-    score += 1
-else:
-    print("Incorrect")
+while True:
+    players = input("Enter the number of players (2 - 5): ")
+    if players.isdigit():
+        players = int(players)
+        if 2 <= players <= 5:
+            break
+        else:
+            print("Must be between 2 - 5 players.")
+    else:
+        print("Invalid, try again.")
 
-answer = input("Question 3 - 22 x 30 = ? ans: ")
-if answer.lower() == "660":
-    print("Correct")
-    score += 1
-else:
-    print("Incorrect")
+max_score = 20
+player_scores = [0 for _ in range(players)]
 
-answer = input("Question 4 - Who is Bangladesh cricket's greatest captain? ans: ")
-if answer.lower() == "mashrafe mortaza":
-    print("Correct")
-    score += 1
-else:
-    print("Incorrect")
+while max(player_scores) < max_score:
+    for player_idx in range(players):
+        print("\nPlayer number", player_idx + 1, "turn has just started!")
+        print("Your total score is:", player_scores[player_idx], "\n")
+        current_score = 0
 
-answer = input("Question 5 - What is the greatest coding institution of Bangladesh? ans: ")
-if answer.lower() == "bsprc":
-    print("Correct")
-    score += 1
-else:
-    print("Incorrect")
+        while True:
+            should_choose = input("Would you like to choose (y)? ")
+            if should_choose.lower() != "y":
+                break
 
-print("Thank you for playing, you got " + str(score) + " questions right")
-print("Thank you for playing, you got " + str(score/4*100) + "% questions right")
+            value = choose()
+            if value == 1:
+                print("You chose a 1! Turn done!")
+                current_score = 0
+                break
+            else:
+                current_score += value
+                print("You chose a:", value)
+
+            print("Your score is:", current_score)
+
+        player_scores[player_idx] += current_score
+        print("Your total score is:", player_scores[player_idx])
+
+max_score = max(player_scores)
+winning_idx = player_scores.index(max_score)
+print("Player number", winning_idx + 1,
+      "is the winner with a score of:", max_score)
